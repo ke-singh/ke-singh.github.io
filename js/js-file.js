0b4e4365b27f709ea7fb70f14df689243a64f882
ke-singh <ke.singh@jacobs-university.de>
2019-10-11 20:14:17 +0200

    Few improvements

--- js/js-file.js
+++ js/js-file.js
@@ -1,68 +1,17 @@
-
 //-----------------------------------------------------------------------//
-
-
 
 //for blur effect
 
-var myElement = $('.top');
-$(window).on('scroll', function() {
-    var st = $(this).scrollTop();
-    myElement.css({
-        'opacity' : 0.2 + st/400
-    });
+var myElement = $(".top");
+$(window).on("scroll", function() {
+  var st = $(this).scrollTop();
+  myElement.css({
+    opacity: 0.2 + st / 400
+  });
 });
-
-var Element = $('.intro');
-$(window).on('scroll', function() {
-    var t = $(this).scrollTop();
-    Element.css({
-        'opacity' : 1 - t/4000
-    });
-});
-
-
-
-//-----------------------------------------------------------------------//
-
-
 
 //for tooltip in social media icons
 
-
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
-  });
-
-
-
-
-
-
-
-
-//-----------------------------------------------------------------------//
-
-// for parallax effect
-
-
-var velocity = 0.5;
-function update(){ 
-      var pos = $(this).scrollTop(); 
-      $('.container1').each(function() { 
-          var $element = $(this);
-          // subtract some from the height b/c of the padding
-          var height = $element.height()-18;
-          $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) + 'px'); 
-      });
-      $('.container2').each(function() { 
-        var $element = $(this);
-        // subtract some from the height b/c of the padding
-        var heigh = $element.height()-18;
-        $(this).css('backgroundPosition', '50% ' + Math.round((heigh - pos) * velocity) + 'px'); 
-    });  
-  };
-$(window).bind('scroll', update);
-  
-
-//-----------------------------------------------------------------------//
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
